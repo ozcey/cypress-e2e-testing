a069331c422b7629885d0492fd5b00b17b7f8fd9
Ryan Ceyhan <ryan.ceyhan@ibm.com>
2024-07-01 11:45:43 -0400

    add test data

--- cypress/e2e/ui_tests/home.cy.js
+++ cypress/e2e/ui_tests/home.cy.js
@@ -1,3 +1,4 @@
+import { cardData } from "../../fixtures/homeData";
 
 describe('Home Tests', () => {
     beforeEach(() => {
@@ -20,38 +21,6 @@ describe('Home Tests', () => {
     });
 
     it('User should see info cards on home page', () => {
-        const cardData = [
-            {
-                card_header: 'Users',
-                card_title: 'Login to your account',
-                card_text: 'Users could log in to account by clicking on Login button',
-                button: {
-                    name: 'Login',
-                    color: 'rgb(0, 123, 255)',
-                    href: '/login'
-                }
-            },
-            {
-                card_header: 'Job Seekers',
-                card_title: 'Launch your career',
-                card_text: 'We train recent immigrants and refugees to adapt their education',
-                button: {
-                    name: 'Apply now',
-                    color: 'rgb(23, 162, 184)',
-                    href: '/applicant/new'
-                }
-            },
-            {
-                card_header: 'Volunteers',
-                card_title: 'Helped Skilled Workers',
-                card_text: 'We collaborate with individuals and partner organizations',
-                button: {
-                    name: 'Sign up',
-                    color: 'rgb(23, 162, 184)',
-                    href: '/volunteer/signup'
-                }
-            }
-        ];
         for (const card of cardData) {
             cy.get('.card-header').should('contain.text', card.card_header);
             cy.get('.card-title').should('contain.text', card.card_title);
---
+++ cypress/fixtures/homeData.js
@@ -0,0 +1,32 @@
+export const cardData = [
+    {
+        card_header: 'Users',
+        card_title: 'Login to your account',
+        card_text: 'Users could log in to account by clicking on Login button',
+        button: {
+            name: 'Login',
+            color: 'rgb(0, 123, 255)',
+            href: '/login'
+        }
+    },
+    {
+        card_header: 'Job Seekers',
+        card_title: 'Launch your career',
+        card_text: 'We train recent immigrants and refugees to adapt their education',
+        button: {
+            name: 'Apply now',
+            color: 'rgb(23, 162, 184)',
+            href: '/applicant/new'
+        }
+    },
+    {
+        card_header: 'Volunteers',
+        card_title: 'Helped Skilled Workers',
+        card_text: 'We collaborate with individuals and partner organizations',
+        button: {
+            name: 'Sign up',
+            color: 'rgb(23, 162, 184)',
+            href: '/volunteer/signup'
+        }
+    }
+];
